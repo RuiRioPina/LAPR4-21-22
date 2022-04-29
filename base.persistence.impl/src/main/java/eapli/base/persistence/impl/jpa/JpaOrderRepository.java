43984658d83dff2d61311dd037f825f2de17b1cb
Rodrigo Rodrigues <1191008@isep.ipp.pt>
2022-04-29 10:39:43 +0100

    #13 [US-1004] [Implementation] Persistence.xml updated for Order and AGV Entities & JpaOrderRepository finished

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaOrderRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaOrderRepository.java
@@ -1,14 +1,43 @@
 package eapli.base.persistence.impl.jpa;
 
+import eapli.base.Application;
 import eapli.base.order.domain.Order;
 import eapli.base.order.repositories.OrderRepository;
+import eapli.framework.domain.repositories.TransactionalContext;
+import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
 
-public class JpaOrderRepository extends BasepaRepositoryBase <Order,Long, Long>
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class JpaOrderRepository extends JpaAutoTxRepository<Order,Long, Long>
         implements OrderRepository {
 
-    public JpaOrderRepository() {
-        super("id");
+    public JpaOrderRepository(final TransactionalContext autoTx) {
+        super(autoTx, "id");
     }
 
+    public JpaOrderRepository(final String puname) {
+        super(puname, Application.settings().getExtendedPersistenceProperties(),
+                "id");
+    }
+
+    @Override
+    public Iterable<Order> findAllActive() {
+        return match("e.systemUser.active = true");
+    }
+
+    @Override
+    public Optional<Order> findById(final Long number) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("number", number);
+        return matchOne("e.id=:number", params);
+    }
+
+    public Optional<Order> findByCustomerId(final Long number) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("number", number);
+        return matchOne("e.customerid=:number", params);
+    }
 }
 
