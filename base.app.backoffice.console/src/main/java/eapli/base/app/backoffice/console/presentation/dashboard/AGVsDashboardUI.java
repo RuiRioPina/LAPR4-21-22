cd297f64af99ea16495cf7718f794838ce42036c
RuiRioPina <1201568@isep.ipp.pt>
2022-05-29 20:35:44 +0100

    #29 [US3001] [Implementation] Trying to fix cloud error.

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/dashboard/AGVsDashboardUI.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/dashboard/AGVsDashboardUI.java
@@ -21,7 +21,7 @@ public class AGVsDashboardUI extends AbstractUI {
         try {
             uri = new URI("http://localhost:11555/"); //porta correta?
             Desktop.getDesktop().browse(uri);
-        } catch ( URISyntaxException | IOException | HeadlessException e) {
+        } catch ( HeadlessException|AWTError | URISyntaxException | IOException e) {
             return false;
         }
         return true;
