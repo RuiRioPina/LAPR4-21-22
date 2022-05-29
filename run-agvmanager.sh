dffb5a0cad60d7dd28542f69cd52c8a2b1af9236
root <root@vs664.dei.isep.ipp.pt>
2022-05-29 17:44:26 +0100

    #31 [US2005] [Implementation] Updating run-agvmanager.sh

--- run-agvmanager.sh
+++ run-agvmanager.sh
@@ -3,7 +3,7 @@
 #REM set the class path,
 #REM assumes the build was executed with maven copy-dependencies
 # shellcheck disable=SC2125
-export BASE_CP=base.app.agvmanager.console/target/agvmanager 1.4.0-SNAPSHOT.jar:base.app.agvmanager.console/target/dependency/*;
+export BASE_CP=base.app.agvmanager.console/target/agvmanager-1.4.0-SNAPSHOT.jar:base.app.agvmanager.console/target/dependency/*;
 
 #REM call the java VM, e.g,
-java -cp $BASE_CP agvmanager.console.TcpServerAGVManager
+java -cp $BASE_CP eapli.base.app.agvmanager.server.TcpServerAGVManager
