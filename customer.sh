dc0eea50b1cf7cf6a8ef3fddeb0e70daa03b402f
jorgefcferreira <1201564@isep.ipp.pt>
2022-06-19 15:24:09 +0100

    #51 [US2006] [Implementation] Cloud fix

--- customer.sh
+++ customer.sh
@@ -3,7 +3,7 @@
 #REM set the class path,
 #REM assumes the build was executed with maven copy-dependencies
 # shellcheck disable=SC2125
-export BASE_CP=base.customer.app/target/agvmanager-1.4.0-SNAPSHOT.jar:base.customer.app/target/dependency/*;
+export BASE_CP=base.customer.app/target/base.customer.app-1.4.0-SNAPSHOT.jar:base.customer.app/target/dependency/*;
 
 #REM call the java VM, e.g,
 java -cp $BASE_CP BaseCustomer
